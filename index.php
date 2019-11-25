30381551103f9f32912c5a37c98d1b1d5cf8af59
Vlad Franco <vladfranco2@gmail.com>
2019-11-24 20:55:55 -0500

    1.0.2 - Removed Submit button, updated README

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-   
+
     <link href="main.css" rel="stylesheet" type="text/css">
 
     <title>Siren</title>
@@ -30,7 +30,6 @@
                			<h2>Siren</h2>
 				<form method="post" action="">
                 			<input class="form-control" type="text" placeholder="Paste a Genius Lyric URL here" aria-label="Search" name="searchlyr"/>
-					<input type="submit" value="Submit" />
 				</form>
 			</div>
 		</div>
@@ -62,4 +61,3 @@
       </div>
   </body>
 </html>
-
